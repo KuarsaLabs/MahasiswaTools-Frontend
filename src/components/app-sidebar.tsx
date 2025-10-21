98a8c3ebe1139ddd6cbf061959f7917aac8e4558
Yoru Akio <yoruakio@proton.me>
2025-10-22 00:36:53 +0700

    fix: removing unused imports

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Brain, Home, Settings } from 'lucide-react';
-import { NavLink, useLocation } from 'react-router-dom';
 
 import { NavMain } from './nav-main';
 import {
